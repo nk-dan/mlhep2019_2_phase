f415170765549ccc8b40d334f9ef9d9f20cb9efc
nk-dan <nikjain@mail.ru>
2020-01-16 16:31:25 +0300

    change to 2 epochs to check

--- solution.py
+++ solution.py
@@ -43,7 +43,7 @@ def add_instance_noise(data, std=0.01):
 def trainer(data_train):
 
     MAX_TRAIN_SIZE = data_train['EnergyDeposit'].shape[0]
-    TRAIN_SIZE = 4000 * 3
+    TRAIN_SIZE = 4000 * 1
     TRAIN_IND_ARR = np.random.choice(MAX_TRAIN_SIZE, TRAIN_SIZE)
 
     EnergyDeposit    = data_train['EnergyDeposit'][TRAIN_IND_ARR].reshape(-1, 1, 30, 30)
@@ -82,7 +82,7 @@ def trainer(data_train):
     discriminator = ModelD()
     generator = ModelGConvTranspose(z_dim=NOISE_DIM)
 
-    epoch_num = 30
+    epoch_num = 2
     lr_dis, lr_gen = 4e-4, 1e-4
 
     g_optimizer = optim.Adam(generator.parameters(), betas=(0.0, 0.999), lr=lr_gen)
